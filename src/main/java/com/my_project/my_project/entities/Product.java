3f059c9ce87a3b9802bd2152faec2f4f2c325a24
Divinity <systemdivinity@gmail.com>
2023-06-03 11:07:19 -0300

    product-order-item

--- src/main/java/com/my_project/my_project/entities/Product.java
+++ src/main/java/com/my_project/my_project/entities/Product.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.JoinColumn;
 
@@ -13,6 +14,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "products")
@@ -24,6 +27,9 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> orderItems = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id  "))
@@ -51,6 +57,15 @@ public class Product implements Serializable {
 
     public String getName() {
         return this.name;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> orders = new HashSet<>();
+        for (OrderItem item : orderItems) {
+            orders.add(item.getOrder());
+        }
+        return orders;
     }
 
     public void setName(String name) {
